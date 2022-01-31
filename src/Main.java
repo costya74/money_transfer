28c6beb1137b19fcd6bf088fc74c83fd26378e15
Бояршинов Константин <costya74@bk.ru>
2022-01-31 15:32:11 +0500

    inition commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int remain = 2_000_000_000;
+        int coming = 500_000_000;
+        int total = remain + coming;
+        System.out.println(total);
+    }
+}
